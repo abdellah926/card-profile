a217c58c0ffe8738b92e00fd63e9a13f4c1e4df2
𝐅𝐀𝐋 | 𝖆𝖇𝖉𝖊𝖑𝖑𝖆𝖍 <abdellahbenka05@gmail.com>
2024-06-17 00:49:34 +0100

    ,

--- apps.js
+++ apps.js
@@ -1,6 +1,7 @@
-document.addEventListener('mousemove',function(e){
-    let dx = e.pageX - window.innerWidth / 2
-    let dy = e.pageY - window.innerHeight / 2
+let profile= document.getElementById('profile-box')
+document.addEventListener('mousemove',function(profile){
+    let dx = profile.pageX - window.innerWidth / 2
+    let dy = profile.pageY - window.innerHeight / 2
     let angleX = 40 * dx / window.innerWidth / 2
     let angleY = 40 * dy / window.innerHeight / 2
     block.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg)`       
